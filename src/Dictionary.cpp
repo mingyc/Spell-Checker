e3c1e4fd15e1d4cad9e991c8910b11598efa0d8f
Ming-Ying Chung <b97037@linux3.csie.ntu.edu.tw>
2011-06-18 22:32:21 +0800

    Start to use fork/pipe communicating between Dictionary & Compressor

--- src/Dictionary.cpp
+++ src/Dictionary.cpp
@@ -1,10 +1,12 @@
 #include "Dictionary.h"
+#include "Compressor.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
 #include <unistd.h>
+#include <sys/wait.h>
 #define  FD_READ  0
 #define  FD_WRITE 1
 
@@ -106,9 +108,17 @@ void Dictionary::read(const string &file) {
 // Load the binary file into internal memory format
 //
 void Dictionary::load(const string &file) {
-  FILE *dictFile = fopen(file.c_str(), "r");
+  //FILE *dictFile = fopen(file.c_str(), "r");
+
   int i;
   char buf[256];
+  int pfd[2];
+  assert(pipe(pfd) != -1);
+
+  // Communicate with the compressor by pipe
+  Compressor::Decompress(file.c_str(), pfd[FD_WRITE]);
+  FILE *dictFile = fdopen(pfd[FD_READ], "r");
+
   fscanf(dictFile, "%d", &length);
   root = new struct Dict[length];
   for(i = 0; i < length; i++){
@@ -128,20 +138,29 @@ void Dictionary::load(const string &file) {
 // Dump internal data structure into this file in binary format
 //
 void Dictionary::dump(const string &file) {
-  FILE *dump = fopen(file.c_str(), "w+");
+  //FILE *dump = fopen(file.c_str(), "w+");
   
-  /*
+  pid_t cpid = 0;
   int pfd[2];
   assert(pipe(pfd) != -1);
-  FILE *dump = fdopen(pfd[FD_WRITE], "w+");
-  */
+  FILE *dump = fdopen(pfd[FD_WRITE], "w");
 
-  root = new struct Dict[length];
-  fprintf(dump, "%d\n", length);
-  preDictDump(preroot, dump);
-  preDictDestroy(preroot);
-  preroot = NULL;
-  fclose(dump);
+
+  if ((cpid = fork()) == 0) { /* child process */
+    // Communicate with the bzip2 compressor by pipe
+    Compressor::Compress(pfd[FD_READ], file.c_str());
+    _exit(EXIT_SUCCESS);
+  }
+  else if (cpid > 0) { /* parent process */
+    root = new struct Dict[length];
+    fprintf(dump, "%d\n", length);
+    preDictDump(preroot, dump);
+    preDictDestroy(preroot);
+    preroot = NULL;
+    fclose(dump);
+  }
+  else { /* error */
+  }
 }
 
 //bool Dictionary::preDictAdd(struct preDict **preroot, char *word){
